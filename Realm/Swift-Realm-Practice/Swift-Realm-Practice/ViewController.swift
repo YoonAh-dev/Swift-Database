bd287d23496ce1dd1ce38aa84ed764efc9385030
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-10 18:11:56 +0900

    [FEAT] CRUD 기능 추가

--- Realm/Swift-Realm-Practice/Swift-Realm-Practice/ViewController.swift
+++ Realm/Swift-Realm-Practice/Swift-Realm-Practice/ViewController.swift
@@ -45,5 +45,45 @@ class ViewController: UIViewController {
             self.label.text = "\(name)님 저장됐습니다!"
         }
     }
+    
+    @IBAction func touchUpRead(_ sender: Any) {
+        // UserInfo Data들을 가져옴
+        let person = realmInstance.objects(UserInfo.self)
+        var personsName = String()
+        
+        for i in 0..<person.count {
+            personsName += "\(person[i].name)\n"
+            self.label.text = personsName
+        }
+    }
+    
+    @IBAction func touchUpUpdate(_ sender: Any) {
+        // %@ 자체가 Objective-C적 notation 같습니다.
+        // %d하거나 %f하는 것과 동일
+        // 같은 이름인 첫번째 사람을 가져와서 update시키는 듯
+        if let userInfo = realmInstance.objects(UserInfo.self).filter(NSPredicate(format: "name = %@", nameTextField.text ?? "No Person")).first {
+                try! realmInstance.write {
+                    userInfo.name = "현빈"
+                    userInfo.age = 38
+                    userInfo.gender = "Male"
+                }
+            
+            self.label.text = "\(nameTextField.text!)가 현빈으로 변경되었습니다."
+        } else {
+            self.label.text = "없습니다! 제대로 넣어주세요!"
+        }
+    }
+    
+    @IBAction func touchUpDelete(_ sender: Any) {
+        if let userInfo = realmInstance.objects(UserInfo.self).filter(NSPredicate(format: "name = %@", nameTextField.text ?? "No Person")).first {
+                try! realmInstance.write {
+                    realmInstance.delete(userInfo)
+                }
+            
+            self.label.text = "삭제되었습니다."
+        } else {
+            self.label.text = "없는 사람입니다."
+        }
+    }
 }
 
